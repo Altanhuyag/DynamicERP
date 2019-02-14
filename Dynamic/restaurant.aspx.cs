fe99ccdd71f0da0dbb55567c48dd76b7bd380b6f
tsegikh <tsenguun.kh@gmail.com>
2019-02-14 19:04:39 +0800

    adding restaurant

---
+++ Dynamic/restaurant.aspx.cs
@@ -0,0 +1,16 @@
+﻿using Dynamic.Models;
+using System;
+using System.Data;
+
+namespace Dynamic
+{
+    public partial class restaurant : System.Web.UI.Page
+    {
+        public static DataTable dtRestaurants;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            dtRestaurants = SystemGlobals.DataBase.ExecuteQuery("spres_RestaurantInfo_SEL").Tables[0];
+        }
+    }
+}
