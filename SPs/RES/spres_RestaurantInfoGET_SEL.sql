c63e9725f5474f5cd7201a0aeb0ac2622c09c1ca
tsegikh <tsenguun.kh@gmail.com>
2019-02-18 14:40:10 +0800

    restaurantiin medeelel unshih avah procedure

---
+++ SPs/RES/spres_RestaurantInfoGET_SEL.sql
@@ -0,0 +1,32 @@
+
+IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'spres_RestaurantInfoGET_SEL')
+DROP PROC spres_RestaurantInfoGET_SEL
+GO
+CREATE PROC spres_RestaurantInfoGET_SEL
+(
+	@XML		  NVARCHAR(MAX),
+    @IntResult    TINYINT		 OUTPUT,
+    @StrResult    NVARCHAR(2000) OUTPUT
+) WITH ENCRYPTION
+AS 
+
+BEGIN
+	SET NOCOUNT ON
+	DECLARE @idoc				Int,			
+			@id					nvarchar(16)
+	EXEC sp_xml_preparedocument @idoc OUTPUT, @XML
+
+	SELECT * INTO #tmp
+		FROM OPENXML (@idoc,'//BusinessObject',2)
+		WITH (  
+				id	nvarchar(16)
+			 )
+	EXEC sp_xml_removedocument @idoc 	
+	
+	SELECT	@id=id
+	FROM #tmp
+
+	select RestaurantPkID, RestaurantName, LogoFile, HeaderText, FooterText, Tax, CityTax, ServiceChargeTax from resRestaurantInfo where RestaurantPkID = @id
+
+END
+GO
